280d2feedfe1eafab11795e5a898efcaedc86574
connorvm <connormannion24@yahoo.com>
2022-09-26 17:57:27 -0400

    commit tree 90416733a97f761da5fc9dc2a6c508ef75ceaaed

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(x) {
+  for (let i = 0; i < x.length; i++) {
+    console.log(`Wrapped ${x[i]} and added a bow!`);
+    
+  }
+
+  return x;
+  }
+
+wrapGifts(gifts);
+
+const friends =["Isaac", "Antonio", "not Nevi"];
+var happysad = ";-;";
+
+function writeCards(names, event) {
+   const cards = [];
+    for (let i = 0; i < names.length; i++) {
+        cards[i] = (`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+   return cards;
+}
+
+writeCards(friends, happysad);
+
+
+
+
+function countDown(pos) {
+    while (pos >= 0 ) {
+        console.log(pos--);
+    }
+}
+
